6ec90712e413f3c5b28960402a25cd2310dfc1b2
= <=>
2022-02-27 16:27:44 +0100

    assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Container,
+  SimpleGrid,
+  Image,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function importAll(r) {
+  return r.keys().map(r);
 }
+
+const imageDirs = [
+  {
+    label: "Chats",
+    path: "../public/assets/images/chats",
+    images: importAll(
+      require.context(
+        "../public/assets/images/chats",
+        false,
+        /\.(png|jpe?g|svg|gif)$/
+      )
+    ),
+  },
+  {
+    label: "Chiens",
+    path: "../public/assets/images/chiens",
+    images: importAll(
+      require.context(
+        "../public/assets/images/chiens",
+        false,
+        /\.(png|jpe?g|svg|gif)$/
+      )
+    ),
+  },
+  {
+    label: "Grenouilles",
+    path: "../public/assets/images/grenouilles",
+    images: importAll(
+      require.context(
+        "../public/assets/images/grenouilles",
+        false,
+        /\.(png|jpe?g|svg|gif)$/
+      )
+    ),
+  },
+  {
+    label: "Panda",
+    path: "../public/assets/images/panda",
+    images: importAll(
+      require.context(
+        "../public/assets/images/panda",
+        false,
+        /\.(png|jpe?g|svg|gif)$/
+      )
+    ),
+  },
+  {
+    label: "Pepe",
+    path: "../public/assets/images/pepe",
+    images: importAll(
+      require.context(
+        "../public/assets/images/pepe",
+        false,
+        /\.(png|jpe?g|svg|gif)$/
+      )
+    ),
+  },
+
+  {
+    label: "Pokémon",
+    path: "../public/assets/images/pokémon",
+    images: importAll(
+      require.context(
+        "../public/assets/images/pokémon",
+        false,
+        /\.(png|jpe?g|svg|gif)$/
+      )
+    ),
+  },
+  {
+    label: "Rats",
+    path: "../public/assets/images/rats",
+    images: importAll(
+      require.context(
+        "../public/assets/images/rats",
+        false,
+        /\.(png|jpe?g|svg|gif)$/
+      )
+    ),
+  },
+];
+
+const App = () => {
+  return (
+    <Container maxW="4xl" centerContent>
+      <Tabs isLazy>
+        <TabList>
+          <SimpleGrid columns={{ base: 3, sm: 4, md: 6, lg: 8, xl: 8 }}>
+            {imageDirs.map(({ label }) => (
+              <Box key={label}>
+                <Tab key={label}>{label}</Tab>
+              </Box>
+            ))}
+          </SimpleGrid>
+        </TabList>
+
+        <TabPanels>
+          {imageDirs.map(({ label, images }) => (
+            <TabPanel key={label}>
+              <SimpleGrid
+                columns={{ base: 3, sm: 3, md: 4, lg: 5, xl: 5 }}
+                spacing={10}
+              >
+                {images.map((src, index) => (
+                  <Box key={index} width={64}>
+                    <Image
+                      src={src}
+                      alt=""
+                      boxSize="64px"
+                      htmlHeight="64px"
+                      // style={minWidth: ""}
+                      loading="lazy"
+                    />
+                  </Box>
+                ))}
+              </SimpleGrid>
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
+    </Container>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import App from "./App";
+// import reportWebVitals from './reportWebVitals';
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode="dark"/>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
